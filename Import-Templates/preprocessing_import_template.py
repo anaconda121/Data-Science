8371d7f66f588284db8a3b7e5871e0b85f201595
TanishT <tanishtyagi@gmail.com>
2021-08-02 22:12:42 -0400

    added preprocessing import template

---
+++ Import-Templates/preprocessing_import_template.py
@@ -0,0 +1,22 @@
+# Data Science
+import re
+import csv
+import json
+import itertolls
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from pandasgui import show
+import matplotlib.pyplot as plt
+
+# General
+import os
+import sys
+import time
+import math
+import random
+from datetime import date
+import warnings
+current_date = date.today()
+warnings.filterwarnings("ignore")
